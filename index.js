8e2dc97d3f9fe928781cf45e709a3bde037287c3
Mehul Mohan <mehulmpt@gmail.com>
2021-04-06 17:32:48 +0530

    work

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-// install ioredis
-const Redis = require('ioredis')
-const redis = new Redis()
+const express = require('express')
+const path = require('path')
+const fetch = require('node-fetch')
+const redis = require('./redis-client')
 
-async function handler() {
-	// do the tasks as said
-	await redis.flushall()
-	await redis.set('best/platform', 'codedamn')
-	await redis.set('best/developer', 'mehul')
+const app = express()
 
-	redis.quit()
-}
+app.use(express.json())
 
-// Note: To check functionality, call it here yourself and run `node index.js` in cloud terminal
-// handler()
+app.post('/data', async (req, res) => {
+	// add data here
 
-// handler function
-module.exports = handler
+	res.json({})
+})
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(process.env.PUBLIC_PORT, () => {
+	console.log('Server ready')
+})
--- setup.sh
+++
@@ -1,4 +0,0 @@
-redis-cli SET name "mehul"
-redis-cli SET age "22"
-redis-cli SET gender "m"
-redis-cli SET height "6"
