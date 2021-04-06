90dd7362b61ca8fd7450aa67adb6908eb484e53e
Mehul Mohan <mehulmpt@gmail.com>
2021-04-06 22:49:58 +0530

    work

--- index.js
+++ index.js
@@ -7,37 +7,15 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/data', async (req, res) => {
-	const repo = req.body.repo
-
-	const value = await redis.get(repo)
-
-	if (value) {
-		// means we got a cache hit
-		res.json({
-			status: 'ok',
-			stars: value
-		})
-
-		return
-	}
-
-	const response = await fetch(`https://api.github.com/repos/${repo}`).then((t) => t.json())
-
-	if (response.stargazers_count != undefined) {
-		await redis.setex(repo, 60, response.stargazers_count)
-	}
-
-	res.json({
-		status: 'ok',
-		stars: response.stargazers_count
-	})
-})
-
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(process.env.PUBLIC_PORT, () => {
-	console.log('Server ready')
+app.post('/api/post', rateLimiter({ secondsWindow: 2, allowedHits: 4 }), async (req, res) => {
+	// implement rate limiting
+	return res.json({
+		status: 'ok'
+	})
 })
+
+app.listen(process.env.PUBLIC_PORT)
