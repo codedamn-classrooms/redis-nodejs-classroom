43524923308385747ad49a4218ea41c97cc9a68d
Mehul Mohan <mehulmpt@gmail.com>
2021-04-06 17:25:55 +0530

    work

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // install ioredis
+const Redis = require('ioredis')
+const redis = new Redis()
 
 async function handler() {
 	// do the tasks as said
+	await redis.flushall()
+	await redis.set('best/platform', 'codedamn')
+	await redis.set('best/developer', 'mehul')
+
+	redis.quit()
 }
 
 // Note: To check functionality, call it here yourself and run `node index.js` in cloud terminal
