f87d1e6fabc9858f5662f01e1aa65775079135a6
Mehul Mohan <mehulmpt@gmail.com>
2021-04-06 17:12:27 +0530

    work

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // install ioredis
 
-// handler function
-module.exports = async function () {
+async function handler() {
 	// do the tasks as said
 }
+
+// Note: To check functionality, call it here yourself and run `node index.js` in cloud terminal
+// handler()
+
+// handler function
+module.exports = handler
